e144c3bffc71e3fccc6b25e9f8e03c26fdc7929d
Yojimbo <pypointereau+dev@gmail.com>
2015-05-30 22:28:51 +1000

    Fixing obselete code in app.js

--- app.js
+++ app.js
@@ -11,33 +11,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/dist'));
 
 app.post('/contact', function (req, res) {	
-	console.log('------------------------ CONTACT ------------------------');
-	
-	// Nodemailer init
-	var smtpTrans = nodemailer.createTransport('SMTP', {
-		service: 'Gmail',
-		auth: { user: process.env.MAILBOXLOGIN, pass: process.env.MAILBOXPASSWORD }
-	});
-
-	// Mail setup
-	// var mailOpts = {
-	// 	from: req.body.name + ' <' + req.body.email + '>', //grab form data from the request body object
-	// 	to: process.env.MAILBOXLOGIN,
-	// 	subject: 'Website contact form',
-	// 	text: req.body.message
-	// };
-
-	console.log(mailOpts);
-	console.log('---------------------------------------------------------');
-	// Sending
-	// smtpTrans.sendMail(mailOpts, function (error, response) {
-	// 	if(error) console.log('Erreur sending mail', error);
-	// 	else console.log('Mail sent');
-	// 	// if(error) 	res.render('contact', {title: 'Raging Flame Laboratory - Contact', msg: 'Error occured, message not sent.', err: true, page: 'contact' });
-	// 	// else 		res.render('contact', { title: 'Raging Flame Laboratory - Contact', msg: 'Message sent! Thank you.', err: false, page: 'contact' });
-	// });
-
-
 
 	// create reusable transporter object using SMTP transport
 	var transporter = nodemailer.createTransport({
@@ -56,12 +29,9 @@ app.post('/contact', function (req, res) {
 
 	// send mail with defined transport object
 	transporter.sendMail(mailOptions, function(error, info){
-	    if(error) return console.log(error);
-	    console.log('Message sent: ' + info.response);
+	    if(error) return console.log('[Nodemailer.sendMail()] Erreur', error);
+	    console.log('[Nodemailer.sendMail()] Message sent: ', info.response);
 	});
-
-
-
 
 });
 
